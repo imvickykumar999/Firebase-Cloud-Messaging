18f9e4bf7b832e6da28493c97a848afdb56f3c2d
Vicky Kumar <imvickykumar999@gmail.com>
2024-07-26 15:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import requests
 from google.oauth2 import service_account
 import google.auth.transport.requests
 
-def get_access_token():
+def get_access_token(secret):
     credentials = service_account.Credentials.from_service_account_file(
-        'influencerhiringmobilevarify-firebase-adminsdk-6u3jg-71f1372357.json',
-        scopes=['https://www.googleapis.com/auth/firebase.messaging']
+    	secret, scopes=['https://www.googleapis.com/auth/firebase.messaging']
     )
     request = google.auth.transport.requests.Request()
     credentials.refresh(request)
     return credentials.token
 
 
-def send_push_notification(token, title, body, image_url):
-    access_token = get_access_token()
-    ProjectID = 'influencerhiringmobilevarify'
+def send_push_notification(token, title, body, image_url, ProjectID, secret):
+    access_token = get_access_token(secret)
     url = f"https://fcm.googleapis.com/v1/projects/{ProjectID}/messages:send"
     
     headers = {
@@ -51,8 +49,14 @@ def send_push_notification(token, title, body, image_url):
 
 if __name__ == "__main__":
     fcm_token = 'cbpdTSSdQEeDkx9_RdYBoC:APA91bHo5uFHWVSwMUpLjRiy6-NN4yu1L0vj5xwHxHvBWzIL66f-tcCHZ5ckomaiyI9fR66ewoqfEjnFktkclpetIlmBRiJ6OAGnhOL3Ng6yOWiq8SgPmS9qEv0ibSOl-GhqY5mbiemW'
+    ProjectID = 'influencerhiringmobilevarify'
+
+    # https://github.com/imvickykumar999/Django-InHouse/blob/main/influencerhiringmobilevarify-firebase-adminsdk-6u3jg-71f1372357.json
+    secret = 'influencerhiringmobilevarify-firebase-adminsdk-6u3jg-71f1372357.json'
+    
     title = 'Hello'
     body = 'World'
-    click_action = 'https://development.influencerhiring.com'  # URL to redirect to on click
-    response = send_push_notification(fcm_token, title, body, click_action)
+    image_url = 'https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white.png'
+    
+    response = send_push_notification(fcm_token, title, body, image_url, ProjectID, secret)
     print('Response:', response)
